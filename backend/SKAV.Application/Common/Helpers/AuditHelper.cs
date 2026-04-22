34f0c6b33f56f044847e79b347bad128f3261663
Fredrik Jonsson <FredrikAndreJonson@hotmail.com>
2026-04-22 10:10:17 +0200

    Add BaseEntity with audit fields and AuditHelper class
    Introduced an abstract BaseEntity class with standard audit properties (CreatedAt, CreatedBy, UpdatedAt, UpdatedBy, DeletedAt, DeletedBy) for entity tracking. Added an AuditHelper static class to set these audit fields conveniently.

---
+++ backend/SKAV.Application/Common/Helpers/AuditHelper.cs
@@ -0,0 +1,25 @@
+﻿using SKAV.Domain.Entities;
+
+namespace SKAV.Application.Common.Helpers
+{
+    public static class AuditHelper
+    {
+        public static void SetCreated(BaseEntity entity, int? userId)
+        {
+            entity.CreatedAt = DateTime.UtcNow;
+            entity.CreatedBy = userId;
+        }
+
+        public static void SetUpdated(BaseEntity entity, int? userId)
+        {
+            entity.UpdatedAt = DateTime.UtcNow;
+            entity.UpdatedBy = userId;
+        }
+
+        public static void SetDeleted(BaseEntity entity, int? userId)
+        {
+            entity.DeletedAt = DateTime.UtcNow;
+            entity.DeletedBy = userId;
+        }
+    }
+}
---
+++ backend/SKAV.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKAV.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public int? CreatedBy { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+        public int? UpdatedBy { get; set; }
+
+        public DateTime? DeletedAt { get; set; }
+        public int? DeletedBy { get; set; }
+    }
+}
