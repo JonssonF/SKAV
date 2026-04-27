3baa0f9957bfed57f5b677dbd6345353929511e7
Fredrik Jonsson <FredrikAndreJonson@hotmail.com>
2026-04-27 13:16:12 +0200

    Instrument.cs

---
+++ backend/SKAV.Domain/Entities/Instrument.cs
@@ -0,0 +1,12 @@
+﻿using SKAV.Domain.Attributes;
+
+namespace SKAV.Domain.Entities
+{
+    [TableName("Instruments")]
+    public class Instrument : BaseEntity
+    {
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public ICollection<MemberInstrument> MemberInstruments { get; set; } = null!;
+    }
+}
