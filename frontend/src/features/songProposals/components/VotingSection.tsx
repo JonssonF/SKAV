f879891c1428eb41fcbabba53f2fc0a980065ae0
Fredrik Jonsson <FredrikAndreJonson@hotmail.com>
2026-05-26 15:39:12 +0200

    warnings fix

--- frontend/src/features/songProposals/components/VotingSection.tsx
+++ frontend/src/features/songProposals/components/VotingSection.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import {
   Container,
-  Title,
   Text,
   SimpleGrid,
   Loader,
--- frontend/src/features/songs/components/MusicSection.tsx
+++ frontend/src/features/songs/components/MusicSection.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import {
   Container,
-  Title,
   Text,
   Card,
   Group,
