874ab401c400d76dacf7cbd68f8730f8240ea770
Fredrik Jonsson <FredrikAndreJonson@hotmail.com>
2026-05-26 15:32:24 +0200

    Update Axios baseURL to use network IP address

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5249/api',
+  baseURL: 'http://192.168.1.207:5249/api',
   headers: {
     'Content-Type': 'application/json',
   },
