d6fc01a1bffdd02424911137c4207b5aa91d858b
Fredrik Jonsson <FredrikAndreJonson@hotmail.com>
2026-05-27 14:56:17 +0200

    Add test step and refactor test infrastructure
    - Added a `dotnet test` step to the deployment pipeline in `deploy.yml` to ensure tests run before deployment.
    - Refactored `GigsControllerTests.cs` and `SmokeTests.cs` to use `CustomWebApplicationFactory` for better test environment configuration.
    - Introduced `CustomWebApplicationFactory.cs` to customize the test environment and set it to "Test".
    - Expanded test coverage in `SmokeTests.cs` by adding new API endpoints and updating authentication logic.
    - Removed unused imports in `GigsControllerTests.cs` to clean up the code.

---
+++ backend/SKAV.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace SKAV.Tests;
+
+public class CustomWebApplicationFactory
+    : WebApplicationFactory<SKAV.Api.Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Test");
+    }
+}
--- backend/SKAV.Tests/Smoke/SmokeTests.cs
+++ backend/SKAV.Tests/Smoke/SmokeTests.cs
@@ -1,37 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
-
-namespace SKAV.Tests.Smoke
+﻿namespace SKAV.Tests.Smoke
 {
-    public class SmokeTests : IClassFixture<WebApplicationFactory<SKAV.Api.Program>>
+    public class SmokeTests : IClassFixture<CustomWebApplicationFactory>
     {
         private readonly HttpClient _client;
 
-        public SmokeTests(WebApplicationFactory<SKAV.Api.Program> factory)
+        public SmokeTests(CustomWebApplicationFactory factory)
         {
             _client = factory.CreateClient();
         }
 
         [Theory]
-        [InlineData("/api/gigs")]
         [InlineData("/api/albums")]
+        [InlineData("/api/gigs")]
         [InlineData("/api/members")]
         [InlineData("/api/songs")]
+        [InlineData("/api/products")]
+        //With Auth
+        [InlineData("/api/booking-recipients")]
         [InlineData("/api/subscribers")]
         [InlineData("/api/users")]
+        [InlineData("/api/product-orders")]
+
 
 
 
         public async Task Endpoints_ShouldBeAlive(string url)
         {
             // Collection to test controllers with authorization
-            var needAuth = new List<string> 
+            var needAuth = new List<string>
             { "/api/subscribers",
+              "/api/booking-recipients",
+              "/api/product-orders",
               "/api/users"
             };
 
             if (needAuth.Contains(url))
             {
-                
+
                 var auth = await _client.GetAsync(url);
                 Assert.Equal(System.Net.HttpStatusCode.Unauthorized, auth.StatusCode);
                 return;
