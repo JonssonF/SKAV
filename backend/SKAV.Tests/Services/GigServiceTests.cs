8f371978065680525eb5471ea40d3b1026cffb2d
Fredrik <Fredrikandrejonson@hotmail.com>
2026-05-29 08:32:36 +0200

    test

--- backend/SKAV.Tests/Services/GigServiceTests.cs
+++ backend/SKAV.Tests/Services/GigServiceTests.cs
@@ -37,7 +37,7 @@ namespace SKAV.Tests.Services
             var result = await service.CreateAsync(_requestDto, default);
 
             // Assert
-            Assert.Equal(45, result.Id);
+            Assert.Equal(12, result.Id);
         }
 
         [Fact]
